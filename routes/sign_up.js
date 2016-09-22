20e2e8ea11dbfd07b2bb7ce8eb83cbdcb5a825fb
Philip Reitz <philipreitz@Philips-MacBook-Air.local>
2016-09-22 11:30:19 -0600

    sign up functionality

--- routes/sign_up.js
+++ routes/sign_up.js
@@ -18,14 +18,14 @@ router.post('/sign_up', function(req, res, next) {
             }).returning('*');
         })
         .then(function(data) {
-          console.log(data);
+          console.log('sign_up.js: data, ', data);
           var profile = {
               id: data[0].id,
               first_name: data[0].first_name,
               last_name: data[0].last_name,
               email: data[0].email
           };
-          console.log(profile);
+          console.log('sign_up.js: profile, ', profile);
           var token = jwt.sign(profile, process.env.SECRET, {expiresIn: 432000});
           res.status(200).json({
               token: token
@@ -35,7 +35,7 @@ router.post('/sign_up', function(req, res, next) {
         .catch(function(err) {
             res.send(err);
         });
-}
+});
 
 
 
